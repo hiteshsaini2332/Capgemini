cbb77c6a06dfed331f7f00a68792f6d40b47ddcb
hiteshsaini2332 <hiteshsaini9466@gmail.com>
2025-12-31 11:39:01 +0530

    Lab1 Two Questions Completed

---
+++ Java/src/programmingpractice/lab1/Question1.java
@@ -0,0 +1,10 @@
+package programmingpractice.lab1;
+
+public class Question1 {
+    public static void main(String[] args) {
+        int birth_year=2000;
+        int current_year=2024;
+        int age=current_year-birth_year;
+        System.out.print("Your age in 2024 is: "+ age);
+    }
+}
