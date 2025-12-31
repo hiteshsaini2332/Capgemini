daa4bfd4f1a2cca0644987f0fcbf48f5fc5458f0
hiteshsaini2332 <hiteshsaini9466@gmail.com>
2025-12-31 10:59:42 +0530

    UseCase 1 Completed

---
+++ Java/src/OopsBannerApp/Usecasefirst.java
@@ -0,0 +1,12 @@
+package OopsBannerApp;
+
+public class Usecasefirst {
+    public static void useCase1() {
+        System.out.println("OOPS");
+    }
+
+
+    public static void main(String[] args) {
+        useCase1();
+    }
+}
